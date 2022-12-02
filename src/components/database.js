5960688259c4926e6d3d15effda0dc9b506e158f
Ihsan-nahid <nahidboss96@gmail.com>
2022-12-02 12:49:10 +0600

    edited database

--- src/components/database.js
+++ src/components/database.js
@@ -6,263 +6,207 @@ export const Leaderboard = [
         year: 2019
     },
     {
-        team_name: "DIU_Whatever",
-        team_member: "DIU_Whatever, DIU_Whatever, Rahim Iqbal",
-        position : 215,
-        year: 2018
-    },
-    {
-        team_name: "DIU_Tr0jAnZ",
-        team_member: "Tanima, Md Erfan, Nazmus Sakib",
-        position : 240,
-        year: 2020
-    },
-
-    
-    {
-        team_name: "DIU_Beli3v3rs",
-        team_member: "DIU_Tr0jAnZ, Md Erfan, Nazmus Sakib",
-        position : 22,
-        img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        year: 2019
-    },
-    {
         team_name: "DIU_Nemesis",
-        team_member: "",
+        team_member: "Muhaiminul Islam Jim, Debashish Saha Pranta, Rifat Islam Robin",
         position : 27,
-        img: "",
         year: 2018
     },
     {
         team_name: "DIU_Beli3v3rs",
-        team_member: "",
+        team_member: "Mahmud Sajjad Abeer, Ahsan Shuvo, Hasibul Hasan Nishat",
         position : 31,
-        img: "",
         year: 2018
     },
     {
         team_name: "DIU_Depuradores",
-        team_member: "",
+        team_member: "Rahat Islam Srijon, Masum Bin Hossain, Saiful Islam Ramim",
         position : 42,
-        img: "",
         year: 2021
     },
     {
         team_name: "DIU_Tr0jAnZ",
-        team_member: "",
+        team_member: "Tanima Hossain, Md Erfan, Nazmus Sakib",
         position : 44,
-        img: "",
         year: 2020
     },
     {
         team_name: "DIU_CURIOSITY",
-        team_member: "",
+        team_member: "Jayanto Mondel, Md Showkat Hossain Chy, Md Mahady Hasan",
         position : 47,
-        img: "",
         year: 2015
     },
-    
     {
         team_name: "DIU_Tortuga",
-        team_member: "",
+        team_member: "DIU_Tortuga, Rahat Islam Srijon, Mohammad A. Naeem",
         position : 48,
-        img: "",
         year: 2020
     },
     {
         team_name: "DIU_HugsForBugs",
-        team_member: "",
+        team_member: "Ishrak Tawsif Nirob, PK Pranto Das, Aumit Hasan Bappy",
         position : 59,
-        img: "",
         year: 2018
     },
     {
         team_name: "DIU_Voids",
-        team_member: "",
+        team_member: "Mohammad A. Naeem, Nazmus Sakib, Shah Habibul Imran",
         position : 60,
-        img: "",
         year: 2019
     },
     {
         team_name: "DIU_Resolvers",
-        team_member: "",
+        team_member: "DIU_Resolvers, Riadh Hasan, Md Galib Hossain",
         position : 63,
-        img: "",
         year: 2021
     },
     {
         team_name: "DIU_ILLATION",
-        team_member: "",
+        team_member: "Monjurul Huda Munna, Md Istiyak Ahmed, Shuvo Karmakar",
         position : 67,
-        img: "",
         year: 2015
     },
     {
         team_name: "DIU_Tr0jAnZ",
-        team_member: "",
+        team_member: "Tanima Hossain, Md Erfan, Nazmus Sakib",
         position : 68,
-        img: "",
         year: 2019
     },
     {
         team_name: "DIU_Primorial",
-        team_member: "",
+        team_member: "Raihanur Rahman, Nesar Ahammed Jony, Mehedi Hasan",
         position : 68,
-        img: "",
         year: 2018
     },
     {
         team_name: "DIU_Iterator",
         team_member: "",
         position : 69,
-        img: "",
         year: 2014
     },
     {
         team_name: "DIU_Sedulous",
-        team_member: "",
+        team_member: "Shimul Sutradhar, Anisur Rahman Sunny, Abu Saleh",
         position : 70,
-        img: "",
         year: 2021
     },
     {
         team_name: "DIU_Beli3v3rs",
-        team_member: "",
+        team_member: "Mahmud Sajjad Abeer, Ahsan Shuvo, Hasibul Hasan Nishat",
         position : 72,
-        img: "",
         year: 2017
     },
     {
         team_name: "DIU_PEKKA",
-        team_member: "",
+        team_member: "Tariq Ahmed Khan, Mohsin Ali, Sadik H Khan",
         position : 72,
-        img: "",
         year: 2014
     },
     {
         team_name: "DIU_PEKKA",
-        team_member: "",
+        team_member: "Tariq Ahmed Khan, Mohsin Ali, Sadik H Khan",
         position : 84,
-        img: "",
         year: 2015
     },
     {
         team_name: "DIU_Recovery",
-        team_member: "",
+        team_member: "Iftekharul Islam Shoccho, Shameem Alam, Zahidul Islam Sumon",
         position : 85,
-        img: "",
         year: 2019
     },
     {
         team_name: "DIU_Jockers",
         team_member: "",
         position : 86,
-        img: "",
         year: 2014
     },
     {
         team_name: "DIU_Lollipop",
         team_member: "",
         position : 87,
-        img: "",
         year: 2014
     },
     {
         team_name: "DIU_Supernova",
         team_member: "",
         position : 88,
-        img: "",
         year: 2014
     },
     {
         team_name: "DIU_CaveDweller",
-        team_member: "",
+        team_member: "Suvrajit Karmaker, Maruf Ahmed, Maruf Ahmed",
         position : 89,
-        img: "",
         year: 2018
     },
     {
         team_name: "DIU_BURNERS",
         team_member: "",
         position : 90,
-        img: "",
         year: 2014
     },
     {
         team_name: "DIU_Asterisk",
         team_member: "",
         position : 91,
-        img: "",
         year: 2016
     },
     {
         team_name: "DIU_CURIOSITY",
-        team_member: "",
+        team_member: "Jayanto Mondel, Md Showkat Hossain Chy, Md Mahady Hasan",
         position : 93,
-        img: "",
         year: 2016
     },
     {
         team_name: "DIU_Thunders",
-        team_member: "",
+        team_member: "Masab Hasnain, Shameem Alam, Nusrat Ullah",
         position : 94,
-        img: "",
         year: 2020
     },
     {
         team_name: "DIU_Undefined_Error",
-        team_member: "",
+        team_member: "Ferdouse Ahmed Foysal, Fuad Wasi, Zakaria Hossain",
         position : 98,
-        img: "",
         year: 2018
     },
     {
         team_name: "DIU_Heuristic",
-        team_member: "",
+        team_member: "Tanzina Rimi, Tanima Hossain, Israt Jahan Fateha",
         position : 103,
-        img: "",
         year: 2018
     },
     {
         team_name: "DIU_INSOMNIACS",
         team_member: "",
         position : 105,
-        img: "",
         year: 2017
     },
     {
         team_name: "DIU_Turtles",
         team_member: "",
         position : 108,
-        img: "",
         year: 2019
     },
     {
         team_name: "DIU_BackslashZero",
-        team_member: "",
+        team_member: "Rajdip Saha, Md Borhan Uddin, Md Mahmudul Hasan",
         position : 109,
-        img: "",
         year: 2015
     },
     {
         team_name: "DIU_Recovery",
-        team_member: "",
+        team_member: "Iftekharul Islam Shoccho, Iftekharul Islam Shoccho, Zahidul Islam Sumon",
         position : 133,
-        img: "",
         year: 2018
     },
     {
         team_name: "DIU_Whatever",
-        team_member: "",
+        team_member: "Rahim Iqbal, Shakhawat Hossain Shawon, হাফিজুর রহমান আরফিন",
         position : 215,
-        img: "",
         year: 2018
     },
     {
         team_name: "DIU_PC_ROCKZ",
-        team_member: "",
+        team_member: "Md. Razibul Hasan Mithu, Md. Razibul Hasan Mithu, Mohammad Shakil Mahmud",
         position : 234,
-        img: "",
         year: 2018
     },
     
